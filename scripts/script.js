ece2e74b058e5aeb1474a92bd64a3bc2a7bb78d0
chernovvova <chernovvova2023@yandex.ru>
2024-04-23 21:04:15 +0300

    add buttons list, hover effect

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,44 @@
-const grid = document.querySelector('.grid');
+const main = document.createElement('div');
+main.classList = 'main';
+main.style.cssText = 'display: flex; justify-content: center; flex-direction: column; gap: 50px; allign-items: center;';
+document.body.appendChild(main);
 
-for(let i = 0; i < 16 * 16; i++) {
-    const gridElem = document.createElement('div');
-    gridElem.textContent = 'Cell';
-    gridElem.style.width = `calc(${100/16}% - 2px)`;
-    gridElem.style.height = `$calc(${100/16}% - 2px)`;
-    gridElem.style.border = '1px black solid';
-    grid.appendChild(gridElem);
+let currentGridSize = 16;
+
+function createGrid(gridSize) {
+    const grid = document.querySelector('.grid');
+
+    for(let i = 0; i < 16 * 16; i++) {
+        const gridElem = document.createElement('div');
+        gridElem.style.width = `calc(${100/16}% - 2px)`;
+        gridElem.style.height = `$calc(${100/16}% - 2px)`;
+        gridElem.style.border = '1px black solid';
+    
+        gridElem.addEventListener('mouseover', (Event) => {
+            Event.target.style.backgroundColor = '#DCD5D7';
+        });
+        grid.appendChild(gridElem);
+    }
 }
+
+const grid = document.createElement('div');
+grid.classList = 'grid';
+grid.style.cssText = `display: flex;
+                     flex-direction: row;
+                     flex-wrap: wrap;
+                     height: 650px;
+                     width: 650px`;
+const gridContainer = document.createElement('div');
+gridContainer.style.cssText = 'display: flex; justify-content: center;';
+gridContainer.appendChild(grid);
+
+const buttons = document.createElement('div');
+buttons.style.cssText = 'display: flex; justify-content: center;';
+main.appendChild(buttons);
+main.appendChild(gridContainer);
+createGrid(currentGridSize);
+
+const changeSizeButton = document.createElement('button');
+changeSizeButton.textContent = 'Change size';
+buttons.appendChild(changeSizeButton);
+
