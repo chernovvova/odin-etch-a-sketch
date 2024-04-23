e2b4824ccea51402b7949ed1f60a8bf637cd1b2b
chernovvova <chernovvova2023@yandex.ru>
2024-04-23 21:22:51 +0300

    add change size button functional

--- scripts/script.js
+++ scripts/script.js
@@ -3,22 +3,33 @@ main.classList = 'main';
 main.style.cssText = 'display: flex; justify-content: center; flex-direction: column; gap: 50px; allign-items: center;';
 document.body.appendChild(main);
 
-let currentGridSize = 16;
+const defaultGridSize = 16;
 
 function createGrid(gridSize) {
     const grid = document.querySelector('.grid');
 
-    for(let i = 0; i < 16 * 16; i++) {
+    for(let i = 0; i < gridSize * gridSize; i++) {
         const gridElem = document.createElement('div');
-        gridElem.style.width = `calc(${100/16}% - 2px)`;
-        gridElem.style.height = `$calc(${100/16}% - 2px)`;
+        gridElem.style.width = `calc(${100 / gridSize}% - 2px)`;
+        gridElem.style.height = `$calc(${100 / gridSize}% - 2px)`;
         gridElem.style.border = '1px black solid';
     
         gridElem.addEventListener('mouseover', (Event) => {
-            Event.target.style.backgroundColor = '#DCD5D7';
+            Event.target.style.backgroundColor = 'green';
         });
         grid.appendChild(gridElem);
     }
+}
+
+function changeSize() {
+    const newSize = prompt('Enter new grid size (number between 0 - 100): ');
+    if(newSize <= 0 || newSize > 100) {
+        alert('Wrong input, enter number between 0 - 100.');
+        return;
+    }
+    const grid = document.querySelector('.grid');
+    grid.replaceChildren();
+    createGrid(newSize);
 }
 
 const grid = document.createElement('div');
@@ -36,9 +47,11 @@ const buttons = document.createElement('div');
 buttons.style.cssText = 'display: flex; justify-content: center;';
 main.appendChild(buttons);
 main.appendChild(gridContainer);
-createGrid(currentGridSize);
+createGrid(defaultGridSize);
 
 const changeSizeButton = document.createElement('button');
 changeSizeButton.textContent = 'Change size';
+changeSizeButton.addEventListener('click', changeSize);
+
 buttons.appendChild(changeSizeButton);
 
